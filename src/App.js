08b0d50fbfc54eb503884de60862f635d2a63c94
Chad <chadndonahue@gmail.com>
2022-10-20 16:17:30 -0700

    started working on api call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react';
+import { commerce } from './lib/commerce';
 import Products from './components/products/Products';
 import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+        
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    console.log(products);
+
   return (
     <div>
         <Navbar />
