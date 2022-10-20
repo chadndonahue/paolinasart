64474af0b5f9967ea1873c6fde1df7998808e054
Chad <chadndonahue@gmail.com>
2022-10-20 14:42:50 -0700

    made products display to page and fixed imports

--- src/components/products/Product/Product.jsx
+++ src/components/products/Product/Product.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Typohraphy, IconButton } from '@material-ui/core';
+import Typohraphy from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
 import { AddShoppingCart } from '@material-ui/icons';
+
 
 import useStyles from './styles';
 
