1a73b322944181a6eaaa1ca608ecf37198165c04
Chad <chadndonahue@gmail.com>
2022-10-20 15:46:53 -0700

    added navbar and hardcoded cart to see if working

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { AppBar, Icon } from '@material-ui/core';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Typography from '@material-ui/core/Typography';
+import { ShoppingCart } from '@material-ui/icons';
+
+import logo from '../../assets/navbar.jpg';
+
+import useStyles from './styles'
+
+const Navbar = () => {
+    const classes = useStyles();
+  return (
+    <div>
+        <AppBar position='fixed' className={classes.AppBar} color='inherit'>
+            <Toolbar>
+                <Typography variant ='h6' className={classes.title} color='inherit'>
+                    <img src={logo} alt='Paolinas Place' height='25px' className={classes.image} />
+                    Paolina's Place
+                </Typography>
+                <div className={classes.grow} />
+                <div className={classes.button}>
+                    <IconButton aria-label='Show cart items' color='inherit'>
+                        <Badge badgeContent={3} color='secondary'>
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+                </div>
+            </Toolbar>
+        </AppBar>
+    </div>
+  )
+}
+
+export default Navbar
--- src/components/products/Product/Product.jsx
+++ src/components/products/Product/Product.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Typohraphy from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -7,7 +8,6 @@ import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import { AddShoppingCart } from '@material-ui/icons';
 
-
 import useStyles from './styles';
 
 
@@ -15,7 +15,7 @@ const Product = ({product}) => {
     const classes = useStyles();
     return (
     <Card className={classes.root}>
-        <CardMedia className={classes.media} image='' title={product.name}/>
+        <CardMedia className={classes.media} image={product.image} title={product.name}/>
         <CardContent>
             <div className={classes.cardContent}>
                 <Typohraphy variant='h5' gutterbottom>
@@ -25,7 +25,7 @@ const Product = ({product}) => {
                     {product.price}
                 </Typohraphy>
             </div>
-            <Typohraphy variant='h2' color='textSecondary'>{product.description}</Typohraphy>
+            <Typohraphy variant='body2' color='textSecondary'>{product.description}</Typohraphy>
         </CardContent>
         <CardActions disableSpacing className={classes.CardActions}>
             <IconButton aria-label='Add to Cart'>
